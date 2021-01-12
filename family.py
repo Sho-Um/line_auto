5ec3f108645842c7dac595a1ef432f2cace31f3d
Sho-Um <zillion.of.zest.stay.gold.sho1@gmail.com>
2021-01-12 18:02:29 +0900

    Create family.py

---
+++ family.py
@@ -0,0 +1,21 @@
+import requests
+# import datetime
+# import pytz
+import os
+
+# time = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+# time = time.strftime('%Y年%m月%d日 %H:%M:%S')
+
+TOKEN = os.environ['LINE_FAMILY_TOKEN']
+api_url = 'https://notify-api.line.me/api/notify'
+send_contents = "\n19時だよ！\n今日も１日おつかれさまです^^"
+
+TOKEN_dic = {'Authorization': 'Bearer' + ' ' + TOKEN}
+send_dic = {'message': send_contents}
+
+# image_file = './sky.jpg'
+# binary = open(image_file, mode='rb')
+# image_dic = {'imageFile': binary}
+
+# requests.post(api_url, headers=TOKEN_dic, data=send_dic, files=image_dic)
+requests.post(api_url, headers=TOKEN_dic, data=send_dic)
